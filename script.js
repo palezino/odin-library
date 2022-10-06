14c0cc576e39c8277e7fa07052a67ed11a48923e
Daniil Panov <from4life@pm.me>
2022-10-06 11:42:34 +0700

    Add read status to a new card

--- script.js
+++ script.js
@@ -45,15 +45,17 @@ document.querySelector('.submit-btn').addEventListener('click', () => {
 
 let booksContainer = document.querySelector('.books-container');
 
-// let bookCard = document.querySelector('.book-card');
+let bookCard = document.querySelector('.book-card');
 
-// console.log(bookCard.childNodes[3].innerText)
+console.log(bookCard.childNodes[7].childNodes[1].childNodes[3].checked)
 
 function createNewCard() {
     let bookCard = document.querySelector('.book-card').cloneNode(true);
     bookCard.childNodes[3].innerText = myLibrary[myLibrary.length - 1]['author'];
     bookCard.childNodes[5].innerText = myLibrary[myLibrary.length - 1]['title'];
     bookCard.childNodes[7].childNodes[3].childNodes[1].innerText = myLibrary[myLibrary.length - 1]['pages'];
+    bookCard.childNodes[7].childNodes[1].childNodes[3].checked = myLibrary[myLibrary.length - 1]['read'];
+
     
     booksContainer.insertBefore(bookCard, addBookBtn);
 
