cd7ff3a03b0f04bd364f3803b3ea2797ab88ffa2
Daniil Panov <from4life@pm.me>
2022-12-11 13:49:53 +0700

    Wrap to rest to IIFE

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// open a new book form
-let addBookBtn = document.querySelector('.add-book');
-addBookBtn.addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
-});
+// // open a new book form
+// let addBookBtn = document.querySelector('.add-book');
+// addBookBtn.addEventListener('click', function() {
+//     document.querySelector('.bg-modal').style.display = 'flex';
+// });
 
-// close a new book form
-let closeFormBtn = document.querySelector('.close')
-closeFormBtn.addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'none';
-})
+// // close a new book form
+// let closeFormBtn = document.querySelector('.close')
+// closeFormBtn.addEventListener('click', function() {
+//     document.querySelector('.bg-modal').style.display = 'none';
+// })
 
 //add a book to the library array
 // let myLibrary = [];
@@ -29,7 +29,7 @@ closeFormBtn.addEventListener('click', function() {
 
 
 // library made with classes
-class Library {
+class Book {
     constructor(author, title, pages, read) {
         this.author = author,
         this.title = title,
@@ -41,45 +41,91 @@ class Library {
 
     static addBookToLibrary() {
         let newBook = document.querySelectorAll('.popup-form');
-        let userInput = new Library(newBook[0].value, newBook[1].value, newBook[2].value, newBook[3].checked);
-        Library.myLibrary.push(userInput);
+        let userInput = new Book(newBook[0].value, newBook[1].value, newBook[2].value, newBook[3].checked);
+     Book.myLibrary.push(userInput);
     }
 }
 
 
+// wrap the rest in a IIFE function
+const library = (() => {
+    // open a new book form
+    let addBookBtn = document.querySelector('.add-book');
+    addBookBtn.addEventListener('click', function() {
+        document.querySelector('.bg-modal').style.display = 'flex';
+    });
 
-
-
-
-document.querySelector('.submit-btn').addEventListener('click', () => {
-    Library.addBookToLibrary();
-    document.querySelector('.bg-modal').style.display = 'none';
-    createNewCard();
-    // console.log(myLibrary);
-});
-
-// creat a new card
-let booksContainer = document.querySelector('.books-container');
-
-function createNewCard() {
-    let bookCard = document.querySelector('.book-card').cloneNode(true);
-    bookCard.childNodes[3].innerText = Library.myLibrary[Library.myLibrary.length - 1]['author'];
-    bookCard.childNodes[5].innerText = Library.myLibrary[Library.myLibrary.length - 1]['title'];
-    bookCard.childNodes[7].childNodes[3].childNodes[1].innerText = Library.myLibrary[Library.myLibrary.length - 1]['pages'];
-    bookCard.childNodes[7].childNodes[1].childNodes[3].checked = Library.myLibrary[Library.myLibrary.length - 1]['read'];
-
+    // close a new book form
+    let closeFormBtn = document.querySelector('.close')
+    closeFormBtn.addEventListener('click', function() {
+        document.querySelector('.bg-modal').style.display = 'none';
+    })
     
-    booksContainer.insertBefore(bookCard, addBookBtn);
+    // add a book to library
+    document.querySelector('.submit-btn').addEventListener('click', () => {
+        Book.addBookToLibrary();
+           document.querySelector('.bg-modal').style.display = 'none';
+           createNewCard();
+           // console.log(myLibrary);
+       });
+    
+    // creat a new card
+    let booksContainer = document.querySelector('.books-container');
 
-    // console.log(myLibrary)
+    function createNewCard() {
+        let bookCard = document.querySelector('.book-card').cloneNode(true);
+        bookCard.childNodes[3].innerText = Book.myLibrary[Book.myLibrary.length - 1]['author'];
+        bookCard.childNodes[5].innerText = Book.myLibrary[Book.myLibrary.length - 1]['title'];
+        bookCard.childNodes[7].childNodes[3].childNodes[1].innerText = Book.myLibrary[Book.myLibrary.length - 1]['pages'];
+        bookCard.childNodes[7].childNodes[1].childNodes[3].checked = Book.myLibrary[Book.myLibrary.length - 1]['read']
+        
+        booksContainer.insertBefore(bookCard, addBookBtn);
 
+        // add possibility to delete new cards
+        document.querySelectorAll('.delete-book').forEach(btn => btn.addEventListener('click', (e) => {
+            e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+            console.log()
+        }));
+    }
+
+    // function to delete existing cards
     document.querySelectorAll('.delete-book').forEach(btn => btn.addEventListener('click', (e) => {
         e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
     }));
-}
+})();
 
-// function to delete existing cards
-document.querySelectorAll('.delete-book').forEach(btn => btn.addEventListener('click', (e) => {
-    e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
-}));
+
+
+
+// document.querySelector('.submit-btn').addEventListener('click', () => {
+//  Book.addBookToLibrary();
+//     document.querySelector('.bg-modal').style.display = 'none';
+//     createNewCard();
+//     // console.log(myLibrary);
+// });
+
+// creat a new card
+// let booksContainer = document.querySelector('.books-container');
+
+// function createNewCard() {
+//     let bookCard = document.querySelector('.book-card').cloneNode(true);
+//     bookCard.childNodes[3].innerText = Book.myLibrary[Book.myLibrary.length - 1]['author'];
+//     bookCard.childNodes[5].innerText = Book.myLibrary[Book.myLibrary.length - 1]['title'];
+//     bookCard.childNodes[7].childNodes[3].childNodes[1].innerText = Book.myLibrary[Book.myLibrary.length - 1]['pages'];
+//     bookCard.childNodes[7].childNodes[1].childNodes[3].checked = Book.myLibrary[Book.myLibrary.length - 1]['read'];
+
+    
+//     booksContainer.insertBefore(bookCard, addBookBtn);
+
+//     // console.log(myLibrary)
+
+//     document.querySelectorAll('.delete-book').forEach(btn => btn.addEventListener('click', (e) => {
+//         e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+//     }));
+// }
+
+// // function to delete existing cards
+// document.querySelectorAll('.delete-book').forEach(btn => btn.addEventListener('click', (e) => {
+//     e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+// }));
 
